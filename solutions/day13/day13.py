fe2ab01b7a04fd40121785a4181d130e3a757efa
kna27 <krisharora27@gmail.com>
2020-12-20 12:42:28 -0500

    Update day13.py

--- solutions/day13/day13.py
+++ solutions/day13/day13.py
@@ -1,7 +1,5 @@
-x= open("data_day13.txt").readlines()
-minTime, s = x[0], x[1]
-
 from functools import reduce
+
 def chinese_remainder(n, a):
     sum = 0
     prod = reduce(lambda a, b: a*b, n)
@@ -9,8 +7,6 @@ def chinese_remainder(n, a):
         p = prod // n_i
         sum += a_i * mul_inv(p, n_i) * p
     return sum % prod
- 
- 
  
 def mul_inv(a, b):
     b0 = b
@@ -22,34 +18,32 @@ def mul_inv(a, b):
         x0, x1 = x1 - q * x0, x0
     if x1 < 0: x1 += b0
     return x1
- 
- 
- 
-if __name__ == '__main__':
-    n = [3, 5, 7]
-    a = [2, 3, 2]
-    print(chinese_remainder(n, a))
 
 # Above is the CRT from rosettacode.org used to answer part 2 of this problem
 
-start, s2 = int(minTime), int(minTime)
-bus = [int(x) if x != "x" else None for x in s.split(",")]
+x= open("data_day13.txt").readlines()
+
+minTime, schedule = int(x[0]), x[1]
+start, s2 = minTime, minTime
+
+busses = [int(x) if x != "x" else None for x in schedule.split(",")]
 
 #Part 1
 f = True
 while f:
-    for m in bus:
-        if m and s2 % m == 0:
-            print("Part 1:", m * (s2 - start))
+    for i in busses:
+        if i and s2 % i == 0:
+            print("Part 1:", i * (s2 - start))
             f = False
-    s2 = s2 + 1
+    s2 += 1
 
+#Part 2
 n = []
 a = []
-for i, m in enumerate(bus):
+for i, m in enumerate(busses):
     if m:
         n.append(m)
         a.append(m-i)
 
-#Part 2
+
 print("Part 2:", chinese_remainder(n, a))
